10cdcad2eb7f3067bdaf610e9877c0dce61cf62e
Coopert <kupchynskyi@gmail.com>
2018-01-02 23:32:24 +0200

    edit blocks

--- dev/js/code.js
+++ dev/js/code.js
@@ -1,4 +1,16 @@
 $(function(){
+	var browserMinWidth = $(document).width()
+	if (browserMinWidth > 800) {
+		$('.block__description').css('height', $('.block__pic').height());
+	}
+	$(window).resize(function() {
+		var browserMinWidth = $(document).width()
+		console.log(browserMinWidth);
+		if (browserMinWidth > 800) {
+			$('.block__description').css('height', $('.block__pic').height());
+		}
+	});
+	//burder bittons and menu
 	$('.bars').click(function(){
 	  $(this).toggleClass('active');
 	  $(".menu").animate(
@@ -7,5 +19,4 @@ $(function(){
 	);
 	})
 
-	$('.block').css('height', $('block__pic').height());
 })
